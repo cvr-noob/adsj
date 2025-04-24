ba16eb93fecd3a2294f765a34c8fb4bb29aaa354
CVR Noob <srichaitanyavictims2023@gmail.com>
2025-04-25 01:31:52 +0530

    Create BST.java

---
+++ BST.java
@@ -0,0 +1,114 @@
+class Node {
+    int value;
+    Node left, right;
+
+    public Node(int value) {
+        this.value = value;
+        left = right = null;
+    }
+}
+
+public class BST {
+    Node root;
+
+    BST() {
+        root = null;
+    }
+
+    void insert(int value) {
+        root = insertRec(root, value);
+    }
+
+    Node insertRec(Node root, int value) {
+        if (root == null) return new Node(value);
+        if (value < root.value)
+            root.left = insertRec(root.left, value);
+        else if (value > root.value)
+            root.right = insertRec(root.right, value);
+        return root;
+    }
+
+    void inorder() {
+        inorderRec(root);
+        System.out.println();
+    }
+
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.value + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    void preorder() {
+        preorderRec(root);
+        System.out.println();
+    }
+
+    void preorderRec(Node root) {
+        if (root != null) {
+            System.out.print(root.value + " ");
+            preorderRec(root.left);
+            preorderRec(root.right);
+        }
+    }
+
+    void postorder() {
+        postorderRec(root);
+        System.out.println();
+    }
+
+    void postorderRec(Node root) {
+        if (root != null) {
+            postorderRec(root.left);
+            postorderRec(root.right);
+            System.out.print(root.value + " ");
+        }
+    }
+
+    boolean search(int value) {
+        return searchRec(root, value);
+    }
+
+    boolean searchRec(Node root, int value) {
+        if (root == null) return false;
+        if (value == root.value) return true;
+        return value < root.value
+                ? searchRec(root.left, value)
+                : searchRec(root.right, value);
+    }
+
+    void delete(int value) {
+        root = deleteRec(root, value);
+    }
+
+    Node deleteRec(Node root, int value) {
+        if (root == null) return null;
+
+        if (value < root.value) {
+            root.left = deleteRec(root.left, value);
+        } else if (value > root.value) {
+            root.right = deleteRec(root.right, value);
+        } else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            root.value = minValue(root.right);
+            root.right = deleteRec(root.right, root.value);
+        }
+
+        return root;
+    }
+
+    int minValue(Node root) {
+        int minVal = root.value;
+        while (root.left != null) {
+            minVal = root.left.value;
+            root = root.left;
+        }
+        return minVal;
+    }
+}
